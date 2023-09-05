09772c683080c3c5fe457cbb083c359e5e7e2265
Rittam <thatv12122003@gmail.com>
2023-09-05 21:27:59 +0700

    git init

---
+++ goodboy/src/main/java/datn/goodboy/GoodboyApplication.java
@@ -0,0 +1,13 @@
+package datn.goodboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodboyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodboyApplication.class, args);
+	}
+
+}
