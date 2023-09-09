2db2c8e624125877373aa9755444d649bb3fe220
nga02 <ngaltph26840@fpt.edu.vn>
2023-09-09 13:30:49 +0700

    entity

---
+++ goodboy/src/main/java/datn/goodboy/model/entity/Image.java
@@ -0,0 +1,13 @@
+package datn.goodboy.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+}
