405bb8ead684261ec654c7cd17f888a92621ecb3
Rittam <thatv12122003@gmail.com>
2023-09-14 15:13:43 +0700

    add fontend

--- goodboy/src/main/java/datn/goodboy/GoodboyApplication.java
+++ goodboy/src/main/java/datn/goodboy/GoodboyApplication.java
@@ -9,5 +9,4 @@ public class GoodboyApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GoodboyApplication.class, args);
 	}
-
 }
--- goodboy/src/main/java/datn/goodboy/controller/DashboardController.java
+++ goodboy/src/main/java/datn/goodboy/controller/DashboardController.java
@@ -1,6 +1,7 @@
 package datn.goodboy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DashboardController {
 
     @GetMapping(value = "dashboard")
-    public ModelMap mmDashboard() {
+    public ModelMap mmDashboard(Model model) {
+        model.addAttribute("message", "hello this is meessage form controller ");
         return new ModelMap();
     }
 
