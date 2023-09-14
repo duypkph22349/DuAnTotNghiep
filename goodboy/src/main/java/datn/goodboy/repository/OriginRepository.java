8eee9e686264dce07175426983d78179f9655b19
nga02 <ngaltph26840@fpt.edu.vn>
2023-09-14 21:26:46 +0700

    vvvv

--- goodboy/src/main/java/datn/goodboy/repository/OriginRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/OriginRepository.java
@@ -1,7 +1,5 @@
 package datn.goodboy.repository;
 
-<<<<<<< HEAD
-import datn.goodboy.model.entity.Brand;
 import datn.goodboy.model.entity.Origin;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OriginRepository extends JpaRepository<Origin, Integer> {
     Page<Origin> findAllByOrderByCreatedAtDesc(Pageable pageable);
-=======
-public interface OriginRepository {
->>>>>>> master
 }
--- goodboy/src/main/java/datn/goodboy/repository/SizeRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/SizeRepository.java
@@ -1,7 +1,5 @@
 package datn.goodboy.repository;
 
-<<<<<<< HEAD
-import datn.goodboy.model.entity.Brand;
 import datn.goodboy.model.entity.Size;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,7 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SizeRepository extends JpaRepository<Size, Integer> {
     Page<Size> findAllByOrderByCreatedAtDesc(Pageable pageable);
-=======
-public interface SizeRepository {
->>>>>>> master
 }
