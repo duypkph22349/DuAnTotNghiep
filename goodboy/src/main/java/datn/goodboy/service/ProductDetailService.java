61227c210a8f20d4f8ff0b60382df9114bbf24df
nga02 <ngaltph26840@fpt.edu.vn>
2023-09-16 00:20:26 +0700

    cc

--- goodboy/src/main/java/datn/goodboy/service/ProductDetailService.java
+++ goodboy/src/main/java/datn/goodboy/service/ProductDetailService.java
@@ -1,11 +1,41 @@
 package datn.goodboy.service;
 
+import datn.goodboy.model.entity.ProductDetail;
 import datn.goodboy.repository.ProductDetailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductDetailService {
     @Autowired
     private ProductDetailRepository productDetailRepository;
+
+    public Page<ProductDetail> findAll(Pageable pageable) {
+        return productDetailRepository.findAllByOrderByCreatedAtDesc(pageable);
+    }
+
+    public ProductDetail add(ProductDetail origin) {
+        return productDetailRepository.save(origin);
+    }
+
+    public ProductDetail update(Integer id, ProductDetail color) {
+        ProductDetail color1 = productDetailRepository.findById(id).get();
+        color1.setName(color.getName());
+        color1.setPrice(color.getPrice());
+        color1.setQuantity(color.getQuantity());
+        color1.setDescription(color.getDescription());
+        color1.setIdProduct(color.getIdProduct());
+        color1.setIdPattern(color.getIdPattern());
+        color1.setIdColor(color.getIdColor());
+        color1.setIdOrigin(color.getIdOrigin());
+        color1.setIdBrand(color.getIdBrand());
+        color1.setIdMaterial(color.getIdMaterial());
+        color1.setIdSize(color.getIdSize());
+        color1.setIdStyles(color.getIdStyles());
+        color1.setStatus(color.getStatus());
+        color1.setUpdatedAt(color.getUpdatedAt());
+        return productDetailRepository.save(color1);
+    }
 }
