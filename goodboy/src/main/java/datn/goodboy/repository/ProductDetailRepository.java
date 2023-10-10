bea3faad45e9b691a541cbb6e37c49c4e937d350
Rittam <thatv12122003@gmail.com>
2023-10-10 21:52:23 +0700

    cc 3

--- goodboy/src/main/java/datn/goodboy/repository/ProductDetailRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/ProductDetailRepository.java
@@ -2,8 +2,8 @@ package datn.goodboy.repository;
 
 import datn.goodboy.model.entity.ProductDetail;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
