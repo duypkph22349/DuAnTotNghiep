bf269d203b03aabb930e630cdd4106393622fadb
VuHa2904 <havmph22172@fpt.edu.vn>
2023-11-12 20:28:24 +0700

    code 12/11

--- goodboy/src/main/java/datn/goodboy/repository/AccountRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package datn.goodboy.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -12,10 +13,12 @@ import org.springframework.data.repository.query.Param;
 import datn.goodboy.model.entity.Account;
 import datn.goodboy.model.entity.Employee;
 import datn.goodboy.model.response.AccountResponse;
+import org.springframework.stereotype.Repository;
 
 /**
  * AccountRepository
  */
+@Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
   @Query(value = "SELECT new datn.goodboy.model.response.AccountResponse(acc.id,acc.customer.name ,acc.customer.phone, acc.email, acc.status) FROM Account acc")
   Page<AccountResponse> getPageAccountRepose(Pageable page);
@@ -25,4 +28,10 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
 
   @Query(value = "SELECT acc FROM Account acc WHERE acc.email LIKE :email")
   List<Account> getAccountByEmail(@Param("email") String email);
+
+
+
+//  Optional<Account> findByEmail(String email);
+//
+//  List<Account> findByIsAdmin(boolean role);
 }
--- goodboy/src/main/java/datn/goodboy/repository/CartDetailRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/CartDetailRepository.java
@@ -2,6 +2,8 @@ package datn.goodboy.repository;
 
 import datn.goodboy.model.entity.CartDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CartDetailRepository extends JpaRepository<CartDetail, Integer> {
 }
