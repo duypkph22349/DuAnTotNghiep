8e2c479daa2d53463b464c28ea1f18016179f3e8
Vu That <92977052+Rittamx031@users.noreply.github.com>
2023-11-21 16:53:22 +0700

    login fix, authen fix

--- goodboy/src/main/java/datn/goodboy/repository/VoucherRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/VoucherRepository.java
@@ -2,6 +2,8 @@ package datn.goodboy.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,10 @@ import datn.goodboy.model.request.VoucherRequest;
 public interface VoucherRepository extends JpaRepository<Voucher, Integer> {
   @Query(value = "SELECT new datn.goodboy.model.request.VoucherRequest(voucher.id ,voucher.name,voucher.start_time ,voucher.end_time ,voucher.quantily ,voucher.discount ,voucher.status ,voucher.types ,voucher.max_discount ,voucher.min_order) FROM Voucher voucher where voucher.id = :id")
   Optional<VoucherRequest> getResponse(@Param("id") int id);
+  @Query("SELECT voucher FROM Voucher voucher " +
+       "WHERE voucher.deleted = false " +
+       "AND voucher.start_time < CURRENT_TIMESTAMP " +
+       "AND (voucher.end_time > CURRENT_TIMESTAMP OR voucher.end_time IS NULL) " +
+       "AND voucher.status = 1")
+Page<Voucher> getAbleVoucher(Pageable pageable);
 }
