4bb0417e3e00c593ea76f3c719612baf39d6aa92
Vu That <92977052+Rittamx031@users.noreply.github.com>
2023-11-21 13:01:48 +0700

    từ bỏ tách loggin

--- goodboy/src/main/java/datn/goodboy/config/SpringSecurityConfig.java
+++ goodboy/src/main/java/datn/goodboy/config/SpringSecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import datn.goodboy.security.repo.AccountInforRepository;
