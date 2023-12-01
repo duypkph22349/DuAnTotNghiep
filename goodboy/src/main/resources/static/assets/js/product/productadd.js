b1399e445f5a3dff275a88e7b871ff5d67d35600
rittam <thatv12122003@gmail.com>
2023-12-01 22:57:13 +0700

    add form add, view product

---
+++ goodboy/src/main/resources/static/assets/js/product/productadd.js
@@ -0,0 +1,43 @@
+// package datn.goodboy.model.request;
+
+// import java.util.List;
+
+// import lombok.AllArgsConstructor;
+
+// public class ProductAddRequest {
+//   String name;
+//   int idOrigin;
+//   int idBrand;
+//   int idMaterial;
+//   int idCategory;
+//   int idStyles;
+//   List<ProductDetailAdd> productDetails;
+//   public class ProductDetailAdd {
+//     float price;
+//     int quantity;
+//     int idSize;
+//     int idPattern;
+//     String description;
+//   }
+// }
+function getLoaiSanPham() {}
+function getNguonGoc() {}
+function getThuongHieu() {}
+function getChatLieu() {}
+function getKieuDang() {}
+function getHoaVan() {}
+function getKichThuoc() {}
+function AddLoaiSanPham() {}
+function AddNguonGoc() {}
+function AddThuongHieu() {}
+function AddChatLieu() {}
+function AddKieuDang() {}
+function AddHoaVan() {}
+function AddKichThuoc() {}
+function validateForm(){}
+function addNewDivHoaVan(){}
+function addNewFormProductDetail(){}
+function saveProduct(){}
+function buildData(){}
+function deleteProductDetail(){}
+function deleteDivHoaVan(){}
