ed701f146e082b10347b39c0174933a50b02ab7a
rittam <thatv12122003@gmail.com>
2023-12-04 12:31:37 +0700

    fix sql

---
+++ fixcode.sql
@@ -0,0 +1,51 @@
+
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC1' ,N'Giảm giá 10% mặt hàng thời trang' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-12-08T00:00:00.000' AS DateTime) ,5 ,40000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,60000 ,400000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC2' ,N'Giảm giá 5% mặt hàng thời trang' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-12-08T00:00:00.000' AS DateTime) ,5 ,10000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,50000 ,200000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC3' ,N'Giảm giá Freeship' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-12-08T00:00:00.000' AS DateTime) ,5 ,30000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,30000 ,30000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC4' ,N'Giảm giá 20% mặt hàng thời trang' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-12-08T00:00:00.000' AS DateTime) ,5 ,20000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,30000 ,99000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC5' ,N'Giảm giá 8% mặt hàng thời trang' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-12-08T00:00:00.000' AS DateTime) ,5 ,24000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,60000 ,300000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC6' ,N'Giảm giá 8% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,24000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,60000 ,300000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC7' ,N'Giảm giá Freeship ' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,30000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,30000 ,30000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC8' ,N'Giảm giá 20% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,20000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,30000 ,99000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC9' ,N'Giảm giá 10% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,40000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,60000 ,400000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC10' ,N'Giảm giá 5% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,10000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,50000 ,200000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC11' ,N'Giảm giá 5% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,10000 ,1 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,50000 ,200000)
+INSERT [dbo].[voucher]
+  ( [code], [name], [start_time], [end_time], [quantily], [discount], [status], [created_at], [update_at], [types], [deleted], [max_discount], [min_order])
+VALUES
+  (N'VC12' ,N'Giảm giá 5% tất cả mặt hàng' ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,5 ,10000 ,0 ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,CAST(N'2023-11-08T00:00:00.000' AS DateTime) ,0 ,0 ,50000 ,200000)
+SET IDENTITY_INSERT [dbo].[voucher] OFF
+GO
