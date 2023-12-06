c5611cdc7db9d41c2b5c9135b9cf6c3574c06eec
Vuha2904 <havmph22172@fpt.edu.vn>
2023-12-07 01:33:08 +0700

    tao hoa don user

--- goodboy/src/main/java/datn/goodboy/controller/usercontroller/CheckOutController.java
+++ goodboy/src/main/java/datn/goodboy/controller/usercontroller/CheckOutController.java
@@ -2,6 +2,9 @@ package datn.goodboy.controller.usercontroller;
 
 import java.util.List;
 
+import datn.goodboy.model.entity.Bill;
+import datn.goodboy.service.BillService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,9 @@ import datn.goodboy.model.entity.Cart;
 import datn.goodboy.model.entity.CartDetail;
 import datn.goodboy.service.CartDetailService;
 import datn.goodboy.service.CartService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CheckOutController {
@@ -20,6 +26,8 @@ public class CheckOutController {
 
     @Autowired
     private CartService cartService;
+    @Autowired
+    private BillService billService;
 
     @GetMapping("/shop/checkout")
     public String viewCheckout(Model model) {
@@ -28,5 +36,15 @@ public class CheckOutController {
         List<CartDetail> cartDetails = cartDetailService.findAllByCartId(cart.getId());
         model.addAttribute("cartDetails", cartDetails);
         return "user/checkout";
+    }
+
+    @PostMapping("/checkout")
+    public String saveCheckOut(@ModelAttribute("bill") Bill bill, HttpServletRequest request) {
+        String money_ship = request.getParameter("money_ship");
+        String total_money = request.getParameter("soTienCanThanhToan");
+        bill.setMoney_ship(Float.parseFloat(money_ship));
+        bill.setTotal_money(Double.valueOf(total_money));
+        billService.saveBill(bill);
+        return "redirect:/home";
     }
 }
