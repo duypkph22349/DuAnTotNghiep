13b267523eada81ea751f3e35fc17d3d67f12047
thinhnxph22307 <thinhnxph22307@fpt.edu.vn>
2024-01-06 22:37:17 +0700

    update bill detail 6/1

--- goodboy/src/main/java/datn/goodboy/repository/BillDetailRepository.java
+++ goodboy/src/main/java/datn/goodboy/repository/BillDetailRepository.java
@@ -1,7 +1,7 @@
 package datn.goodboy.repository;
 
-import datn.goodboy.model.entity.Product;
-import datn.goodboy.model.entity.ProductDetail;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,6 +16,7 @@ public interface BillDetailRepository extends JpaRepository<BillDetail, Integer>
     @Query("SELECT bd FROM BillDetail bd WHERE bd.idBill.id =:idBill AND bd.productDetail.id =:idProduct")
     BillDetail findByIdBillAndIdProduct(int idBill, int idProduct);
 
-    @Query("SELECT bd.productDetail, SUM(bd.quantity) AS totalQuantity FROM BillDetail bd GROUP BY bd.productDetail")
-    List<ProductDetail> countTotalQuantityByProductDetail();
+    @Query("SELECT bd FROM BillDetail bd WHERE bd.idBill.id =:idBill")
+    List<BillDetail> findByIdListBill(int idBill);
+
 }
