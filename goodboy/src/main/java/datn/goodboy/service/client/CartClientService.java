d2adef37b3b4093014aa10d7251cad44025e4721
ngalt <=>
2024-01-08 15:15:21 +0700

    [FIX] fix template checkout

--- goodboy/src/main/java/datn/goodboy/service/client/CartClientService.java
+++ goodboy/src/main/java/datn/goodboy/service/client/CartClientService.java
@@ -1,23 +1,14 @@
 package datn.goodboy.service.client;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import datn.goodboy.model.entity.Account;
 import datn.goodboy.model.entity.Cart;
 import datn.goodboy.repository.CartRepository;
 import datn.goodboy.security.service.AccountInfoService;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class CartClientService {
@@ -27,11 +18,6 @@ public class CartClientService {
 
     @Autowired
     private AccountInfoService userService;
-
-    @Autowired
-    private HttpServletRequest request;
-
-    private static final String CART_COOKIE_NAME = "userCart";
 
     public Integer getQuantityByIDCustomer(){
         Integer quantity = 0;
@@ -45,38 +31,11 @@ public class CartClientService {
         }else{
             //GET QUANTITY NO LOGIN
             quantity = 0;
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null) {
-                for (Cookie cookie : cookies) {
-                    if (CART_COOKIE_NAME.equals(cookie.getName())) {
-                        if(cookie.getValue() != null){
-                            if(deserializeCart(cookie.getValue()) != null){
-                                for(Integer i : deserializeCart(cookie.getValue()).values()){
-                                    quantity += i;
-                                }
-                            }
-                        }
-
-                    }
-                }
-            }
         }
         return quantity;
     }
 
     public Object findVoucherByCode(String code) {
         return cartRepository.findVoucherByCode(code);
-    }
-
-    private Map<Integer, Integer> deserializeCart(String cartValue) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            String decodedValue = URLDecoder.decode(cartValue, "UTF-8");
-            return objectMapper.readValue(decodedValue, new TypeReference<HashMap<Integer, Integer>>() {
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 }
